7635580a0d4560721f010c2767d2dce3fbd0b5b3
avk1702 <avk1702@bk.ru>
2023-02-22 16:12:43 +0500

    решение с void Main

---
+++ task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    //Задача 29: Напишите метод, который задаёт массив из N элементов 
+    // и выводит их на экран.
+    // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+    // 6, 1, 33 -> [6, 1, 33]
+    int size, index;
+    Console.Write("Введите количество элементов в массиве:  ");
+    size = Convert.ToInt32(Console.ReadLine());
+    index = 0;
+
+    while (index < size)
+    {
+      int[] col = new int[size];
+      Random rand = new Random();
+      col[index] = rand.Next(10);
+      Console.Write(col[index].ToString() + " ");
+      index = index + 1;
+    }
+  }
+}
+
+
+
+
+
