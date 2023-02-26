2d6b413aee6e14faec21d89cbdabc53fb270caf0
avk1702 <avk1702@bk.ru>
2023-02-26 11:51:34 +0500

    решение с int методом

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -1,18 +1,17 @@
 ﻿// Задача 27: Напишите метод, который принимает на вход число и 
 // выдаёт сумму цифр в числе. 452 -> 11 82 -> 10 9012 -> 12
 
-internal class Program
+int SummAll (int number)
 {
-  private static void Main(string[] args)
-  {
-    Console.Write("Введите число:  ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    int sum = 0;
+  int sum = 0;
     while (number > 0)
     {
       sum += number % 10;
       number = number / 10;
     }
-    Console.WriteLine($" Сумма всех цифр в числе " + sum);
-  }
-}
+    return sum;
+} 
+    
+Console.Write("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Сумма всех цифр в числе " + (SummAll(number)));
