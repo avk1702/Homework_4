4a34c2d20d48c255115e4cf8ccb1f7cf699998b1
avk1702 <avk1702@bk.ru>
2023-02-26 11:24:27 +0500

    решение без метода

---
+++ task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и 
+// выдаёт сумму цифр в числе. 452 -> 11 82 -> 10 9012 -> 12
+
+Console.Write("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (number > 0)
+{
+  sum += number % 10;
+  number = number / 10;
+}
+Console.WriteLine($" Сумма всех цифр в числе " + (sum));
+
+
